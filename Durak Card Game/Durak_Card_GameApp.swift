0e85b0b6549325d5c84bdc18f63c31993c7267b7
guyfromplanetearth <guyfromplanetearth@gmail.com>
2021-07-17 14:55:24 +0300

    Initial Commit

---
+++ Durak Card Game/Durak_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Durak_Card_GameApp.swift
+//  Durak Card Game
+//
+//  Created by Nazar Kostiv on 17.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct Durak_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
